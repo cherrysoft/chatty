8b3cf2804f05911077a75c3f85259645a2e71263
cherrysoft <michael@cherrysoft.com>
2014-05-17 23:13:48 +0100

    adding working yoda functionality

--- server.js
+++ server.js
@@ -5,13 +5,32 @@ var express = require('express'),
 
 dotenv.load();
 
+var mashapeTestingKey = process.env.MASHAPE_TESTING_KEY;
+
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 
 app.post('/say/something', function(req, res) {
   var something = req.body.say;
 
+  console.log(mashapeTestingKey);
+
+
   var input = 'https://jeannie.p.mashape.com/text/?input=' + something + '&locale=en&location=50.3%2C9.0&clientFeatures=all&timeZone=%2B120&out=simple%2Fjson';
+
+  var Request = unirest.get(input)
+    .headers({ 
+      "X-Mashape-Authorization": mashapeTestingKey
+  })
+  .end(function (response) {
+    res.send(response.body);
+  });
+});
+
+app.post('/yoda/translate', function(req, res) {
+  var something = req.body.say;
+
+  var input = 'https://yoda.p.mashape.com/yoda?sentence=' + something;
 
   var Request = unirest.get(input)
     .headers({ 
@@ -21,7 +40,6 @@ app.post('/say/something', function(req, res) {
     res.send(response.body);
   });
 });
-
 
 app.get('*',function(req,res){
   res.sendfile('public/static/index.html');
