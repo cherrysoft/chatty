9b9c2aab01a88c366df6dcbd69962d0adabda4d1
cherrysoft <michael@cherrysoft.com>
2014-05-17 17:25:53 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express'),
+    app = express(),
+    unirest = require('unirest');
+
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/public'));
+
+app.post('/say/something', function(req, res) {
+  var something = req.body.say;
+
+  var input = 'https://jeannie.p.mashape.com/text/?input=' + something + '&locale=en&location=50.3%2C9.0&clientFeatures=all&timeZone=%2B120&out=simple%2Fjson';
+
+  var Request = unirest.get(input)
+    .headers({ 
+      "X-Mashape-Authorization": process.env.MASHAPE_TESTING_KEY
+  })
+  .end(function (response) {
+    res.send(response.body);
+  });
+});
+
+
+app.get('*',function(req,res){
+  res.sendfile('public/static/index.html');
+});
+
+var server = app.listen(3000, function() {
+  console.log('listening on port %d', server.address().port);
+});
